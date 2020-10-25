ff22e9273f445253e049e4da28da78070fab5f42
dave gumba <dave_gumba@yahoo.com>
2020-10-25 01:25:55 -0400

    gonna change it to width instead of height

---
+++ collect.py
@@ -0,0 +1,63 @@
+from PIL import Image
+
+def merge_two_images(file1, file2):
+    """Merge two images into one, displayed side by side
+    :param file1: path to first image file
+    :param file2: path to second image file
+    :return: the merged Image object
+    """
+    image1 = Image.open(file1)
+    image2 = Image.open(file2)
+
+    (width1, height1) = image1.size
+    (width2, height2) = image2.size
+
+    result_width = width1 + width2
+    result_height = max(height1, height2)
+
+    result = Image.new('RGB', (result_width, result_height))
+    result.paste(im=image1, box=(0, 0))
+    result.paste(im=image2, box=(width1, 0))
+    return result
+
+def merge_images(files):
+    """Merge two images into one, displayed side by side
+    :param file1: path to first image file
+    :param file2: path to second image file
+    :return: the merged Image object
+    """
+    result_width = 0
+    heights = []
+    images = []
+    for file in files: 
+      image = Image.open(file)
+      images.append(image)
+      (width, height) = image.size
+      result_width = result_width + width
+      heights.append(height)
+
+    result_height = max(heights)
+
+    result = Image.new('RGB', (result_width, result_height))
+    
+    image_position = (0, 0)
+    for image in images:
+      result.paste(im=image, box=image_position)
+      image_position = (image_position[0] + width, 0)
+
+    return result
+
+def create_merged(show_title, num_seasons):
+  query_search = show_title + "S"
+  title = query_search[:-1]
+  range_to_use = num_seasons + 1
+  images_to_merge = [query_search + str(x) + ".png" for x in range(1, range_to_use)]
+  merged = merge_images(images_to_merge)
+  file_name_merged = f'{title}Merged.png'
+  print("Created " + file_name_merged)
+  merged.save(file_name_merged, 'PNG')
+
+shows = [["Fleabag", 2], ["Barry", 2], ["Black Mirror", 5], ["High Maintenance", 4]]
+
+for show in shows:
+  create_merged(show[0], show[1])
--- main.py
+++ main.py
@@ -1,41 +1,58 @@
 '''
 Parameters:
-- 
+- show url
+- show title
+- show season
 
 '''
 
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import csv
+import os
+
+def plot_graph(title, season, url):
+  print(url)
+  result = requests.get(url)
+  soup = BeautifulSoup(result.content, 'html.parser')
+
+  ratings_html = soup.find_all('span', class_='ipl-rating-star__rating')
+  ratings = []
+  for rating in ratings_html:
+    ratings.append(rating.get_text())
+
+  y_axis_title = "Rating"
+  x_axis_title = "Episode #"
+
+  show_title = title
+  show_season = season
+  graph_title = f'{show_title} (Season {show_season})'
+
+  file_extension = ".png"
+  file_name_to_save = f'{show_title}S{show_season}{file_extension}'
+  ratings = [float(rating) for rating in ratings if '.' in rating]
+  num_episodes = len(ratings)
+  episode_numbers = [x for x in range(1, num_episodes + 1)]
+
+  plt.suptitle(graph_title, fontsize=16)
+  plt.plot(episode_numbers, ratings)
+  plt.ylabel(y_axis_title)
+  plt.xlabel(x_axis_title)
+
+  plt.savefig(file_name_to_save)
+  plt.show()
+
+csv_file = "shows.csv"
+
+with open(csv_file) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    header = next(readCSV)
+    for row in readCSV:
+        title = row[0]
+        season = row[1]
+        url = row[2]
+        plot_graph(title, season, url)
 
 
 
-url = "https://www.imdb.com/title/tt5348176/episodes?season=1&ref_=tt_eps_sn_1"
-result = requests.get(url)
-soup = BeautifulSoup(result.content, 'html.parser')
-
-ratings_html = soup.find_all('span', class_='ipl-rating-star__rating')
-ratings = []
-for rating in ratings_html:
-  ratings.append(rating.get_text())
-
-y_axis_title = "Rating"
-x_axis_title = "Episode #"
-
-show_title = "Barry"
-show_season = "1"
-graph_title = f'{show_title} (Season {show_season})'
-
-file_extension = ".png"
-file_name_to_save = show_title + file_extension
-ratings = [float(rating) for rating in ratings if '.' in rating]
-num_episodes = len(ratings)
-episode_numbers = [x for x in range(1, num_episodes + 1)]
-
-plt.suptitle(graph_title, fontsize=16)
-plt.plot(episode_numbers, ratings)
-plt.ylabel(y_axis_title)
-plt.xlabel(x_axis_title)
-
-plt.savefig(file_name_to_save)
-plt.show()
